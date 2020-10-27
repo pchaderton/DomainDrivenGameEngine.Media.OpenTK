e2185c0b74544141452fc38561b690adb2cb9ea0
Preston Chaderton <pchaderton@gmail.com>
2020-10-27 19:02:09 -0400

    Adding a new implementation and loading service for loading sound effects.

---
+++ src/DomainDrivenGameEngine.Media.OpenTK/Models/LoadedSoundEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DomainDrivenGameEngine.Media.Models;
+
+namespace DomainDrivenGameEngine.Media.OpenTK.Models
+{
+    /// <summary>
+    /// A loaded sound effect for use with OpenTK.
+    /// </summary>
+    public class LoadedSoundEffect : IMediaImplementation
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadedSoundEffect"/> class.
+        /// </summary>
+        /// <param name="bufferId">The OpenAL buffer ID referring to the loaded sound effect.</param>
+        public LoadedSoundEffect(int bufferId)
+        {
+            if (bufferId <= 0)
+            {
+                throw new ArgumentException($"A valid {nameof(bufferId)} is required.");
+            }
+
+            BufferId = bufferId;
+        }
+
+        /// <summary>
+        /// Gets the OpenAL buffer ID referring to the loaded sound effect.
+        /// </summary>
+        public int BufferId { get; }
+    }
+}
---
+++ src/DomainDrivenGameEngine.Media.OpenTK/Services/SoundEffectLoadingService.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using DomainDrivenGameEngine.Media.Models;
+using DomainDrivenGameEngine.Media.OpenTK.Models;
+using DomainDrivenGameEngine.Media.Services;
+using OpenTK.Audio.OpenAL;
+
+namespace DomainDrivenGameEngine.Media.OpenTK.Services
+{
+    /// <summary>
+    /// A service for loading sound effects.
+    /// </summary>
+    public class SoundEffectLoadingService : BaseMediaLoadingService<SoundEffect, LoadedSoundEffect>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SoundEffectLoadingService"/> class.
+        /// </summary>
+        /// <param name="sources">The <see cref="IMediaSourceService{SoundEffect}"/>s to use for sourcing sound effects.</param>
+        public SoundEffectLoadingService(IMediaSourceService<SoundEffect>[] sources)
+            : base(sources)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override LoadedSoundEffect LoadImplementation(params SoundEffect[] media)
+        {
+            var soundEffect = media.First();
+
+            var bufferId = AL.GenBuffer();
+
+            AL.BufferData(bufferId,
+                          soundEffect.Channels == 2 ? ALFormat.Stereo16 : ALFormat.Mono16,
+                          soundEffect.Bytes,
+                          soundEffect.SampleRate);
+
+            return new LoadedSoundEffect(bufferId);
+        }
+
+        /// <inheritdoc/>
+        protected override void UnloadImplementation(LoadedSoundEffect implementation)
+        {
+            AL.DeleteBuffer(implementation.BufferId);
+        }
+    }
+}
